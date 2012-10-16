29b93545e49bf3375cceb1aded2d5103280c3ae1
Iñaki Garay <igarai@gmail.com>
2012-10-15 23:52:25 -0300

    Added SymbolTable and OurSymbol classes.

---
+++ src/OurSymbol.java
@@ -0,0 +1,18 @@
+public class OurSymbol {
+    
+    private String type;
+    private String name;
+    private Integer value;
+
+    public OurSymbol(String type, String name, Integer value) {
+        this.type = type;
+        this.name = name;
+        this.value = value;
+    }
+
+    public String  getName()  { return name;  }
+    public String  getType()  { return type;  }
+    public Integer getValue() { return value; }
+
+    public void setValue(Integer v) { this.value = v; }
+}
---
+++ src/SymbolTable.java
@@ -0,0 +1,80 @@
+import java.security.SecureRandom;
+import java.math.BigInteger;
+import java.util.LinkedList;
+
+public class SymbolTable {
+    private LinkedList<OurSymbol> table;
+    private int lastAnon;
+
+    public SymbolTable() {
+        lastAnon = 0;
+        table    = new LinkedList<OurSymbol>();
+    }
+
+    // 0: Ok
+    // 1: Variable ya declarada del mismo tipo
+    // 2: Variable declarada de distinto tipo
+    public int addVar(String type, String name, Integer value) {
+        for(int i = 0; i < table.size(); i++) {
+            if(table.get(i).getName().compareToIgnoreCase(name) == 0 &&
+               table.get(i).getType().equals(type))
+                return 1;
+            if(table.get(i).getName().compareToIgnoreCase(name) == 0 &&
+               !(table.get(i).getType().equals(type)))
+                return 2;
+        }
+
+        table.add(new OurSymbol(type, name, value));
+        return 0;
+    }
+
+    public boolean isDeclared(String name) {
+        for(int i = 0; i < table.size(); i++) {
+            if(table.get(i).getName().compareToIgnoreCase(name) == 0)
+                return true;
+        }
+
+        return false;
+    }
+
+    public OurSymbol get(String name) {
+        for(int i = 0; i < table.size(); i++) {
+            if(table.get(i).getName().compareToIgnoreCase(name) == 0)
+                return table.get(i);
+        }
+
+        return null;
+    }
+
+    public Integer getValue(String name) {
+        for(int i = 0; i < table.size(); i++) {
+            if(table.get(i).getName().compareToIgnoreCase(name) == 0)
+                return table.get(i).getValue();
+        }
+
+        return null;
+    }
+
+    public void setValue(String name, Integer value) {
+        int pos = -1;
+        for(int i = 0; i < table.size(); i++) {
+            if(table.get(i).getName().compareToIgnoreCase(name) == 0) {
+                pos = i;
+                break;
+            }
+        }
+
+        if(pos == -1)
+            return;
+
+        table.get(pos).setValue(value);
+    }
+
+    public String addAnonymVar(String type, Integer value) {
+        int res = 1;
+        String name = "__anon" + lastAnon++;
+
+        res = addVar(type, name, value);
+        return name;
+    }
+}
